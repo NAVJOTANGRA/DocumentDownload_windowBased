d2ab8b6fe2ed219979354917e95c6bd12aba299b
nangra <nangra@NAVJOT>
2018-07-25 11:11:31 +0530

    done

--- DownloadDocuments/Form1.Designer.cs
+++ DownloadDocuments/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Click To Download All Documents";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Download
+            // 
+            this.Download.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Download.Location = new System.Drawing.Point(26, 92);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(100, 23);
+            this.Download.TabIndex = 1;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Download);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Download;
     }
 }
 
--- DownloadDocuments/Form1.cs
+++ DownloadDocuments/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,41 @@ namespace DownloadDocuments
 {
     public partial class Form1 : Form
     {
+        string connectionstring = ConfigurationManager.ConnectionStrings["SQLConnectionString1"].ConnectionString;
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void Download_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get Data from Database 
+                DataTable dttable = GetDataFromDatabase();
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        
+        //Get Data from Procedure
+        public DataTable GetDataFromDatabase()
+        {
+            DataView dtview = new DataView();
+            dtview.Sort = "TermianlId";
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connectionstring;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("[dbo].[GetAllDocumentsIds]", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            sd.Fill(dt);
+            return dt;
         }
     }
 }
